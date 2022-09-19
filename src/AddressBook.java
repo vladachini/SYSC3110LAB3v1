3e3afff5055ab02e20986f3d9ff06c8d2d1d7bb3
Vladimir Kovacina <vladimirkovacina@yahoo.com>
2022-09-19 13:16:13 -0400

    Adding Buddy and removing buddy from addressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,5 +16,9 @@ public class AddressBook {
     }
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom","Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
